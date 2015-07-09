0980d332bc9aef097801b15ba3d6f4e7f00d8a22
Yuanfei Xu <yuanfei@uoregon.edu>
2015-07-09 16:59:09 -0700

    Revert "restart"
    This reverts commit 1015c5aa3da9749afab9a4a3d27e01b263a56984.

---
+++ manifests/site.pp
@@ -0,0 +1,13 @@
+node ip-10-0-7-186 {
+	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
+    }
+
+node ip-10-0-7-249 {
+    }
+
+node ip-10-0-7-211 {
+    }
