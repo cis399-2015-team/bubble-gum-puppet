8158ffc474001c7c5841fe8b423b8d364262a669
Yuanfei Xu <yuanfei@uoregon.edu>
2015-07-10 12:03:52 -0700

    add
    add

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,6 +9,6 @@ node ip-10-0-7-86 {
     	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
 	    user    => root,
-	    minute  => "*/5",
+	    minute  => "*/1",
 	}
     }
