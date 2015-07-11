519e27d21be240d5c5f1135382995e10aeba6d61
Yuanfei Xu <yuanfei@uoregon.edu>
2015-07-10 18:44:31 -0700

    ad
    dd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,13 @@
+ # update /etc/puppet on puppet master (use puppet master node name)
  node ip-10-0-7-34 {
+    	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
+    }
+ node ip-10-0-7-35 {
     }
 
-    node ip-10-0-7-35 {
-    }
-
-    node ip-10-0-7-36 {
+ node ip-10-0-7-36 {
     }
