b961922cd3a7a1605c1fe7645d4f74b1902962ea
Yuanfei Xu <yuanfei@uoregon.edu>
2015-07-17 17:26:16 -0700

    apache2 things
    things

---
+++ modules/apache2/manifests/init.pp
@@ -0,0 +1,30 @@
+class apache2 {
+    package {
+        'apache2': ensure => installed;
+    }
+
+    file { '/etc/apache2/apache2.conf':
+        source  => 'puppet:///modules/apache2/apache2.conf',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['apache2'],
+    }
+
+    file { '/var/www/html/foo.html':
+        source  => 'puppet:///modules/apache2/foo.html',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['apache2'],
+    }
+
+    service { 'apache2':
+        enable    => true,
+        ensure    => running,
+        require   => [ Package['apache2'],
+                       File['/etc/apache2/apache2.conf'],
+                       File['/var/www/html/foo.html']],
+        subscribe => File['/etc/apache2/apache2.conf'],
+    }
+}
