cb93f13d4f893e6a010c216fdee57c94991b6015
Yuanfei Xu <yuanfei@uoregon.edu>
2015-08-10 20:32:01 -0700

    fix php
    fix

--- modules/php5/manifest/init.pp
+++ modules/php5/manifest/init.pp
@@ -2,7 +2,14 @@ class php5 {
 	package{ "php5": ensure => installed;}
         package{ "libapache2-mod-php5": ensure => installed;}
         package{ "php5-mycrypt": ensure => installed;}
-
+	
+	file { "/var/www/html/index.php":
+		source	=> ["puppet:///modules/php5/index.php",
+			   ],
+		mode 	=> 444,
+		owner	=> root,
+		group	=> root,
+	}
 
         service{ "php5":
                   enable  => true,
