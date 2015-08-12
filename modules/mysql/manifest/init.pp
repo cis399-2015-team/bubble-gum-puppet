7071a5a04f3890dbed0eb1317b2f133fba3ad195
Yuanfei Xu <yuanfei@uoregon.edu>
2015-08-11 17:27:56 -0700

    test mysql
    mysql

--- modules/mysql/manifest/init.pp
+++ modules/mysql/manifest/init.pp
@@ -1,7 +1,6 @@
 class mysql {
-    package{ "php5-mysql": ensure => installed;}
-    package{ "mysql-server": ensure => installed;}
-
+    package{ "mysql": ensure => installed;}
+    
     file { '/etc/mysql/my.cnf':
         source  => 'puppet:///modules/mysql/my.cnf',
         mode    => 644,
@@ -10,14 +9,10 @@ class mysql {
         require => Package["mysql-server"],Package["php5-mysql"],
     }
 
-   service{ "mysql-server":
+   service{ "mysql":
                   enable  => true,
                   ensure  => running,
-                  require => Package["mysql-server"],
+                  require => Package["mysql"],
         }
-	service{ "php5-mysql":
-                  enable  => true,
-                  ensure  => running,
-                  require => Package["php5-mysql"],
-        }
+	
 }
