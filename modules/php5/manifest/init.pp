8c21d67585b191342d674b5db8ec3c3412ed9a59
Yuanfei Xu <yuanfei@uoregon.edu>
2015-08-11 17:17:39 -0700

    test php5
    test php5

--- modules/php5/manifest/init.pp
+++ modules/php5/manifest/init.pp
@@ -1,29 +1,12 @@
 class php5 {
 	package{ 'php5': ensure => installed;}
-        package{ 'libapache2-mod-php5': ensure => installed;}
-        package{ 'php5-mycrypt': ensure => installed;}
+      
 	
 	file { "/var/www/html/index.php":
-		source	=> ["puppet:///modules/php5/index.php",
-			   ],
+		source	=> ["puppet:///modules/php5/index.php"],
 		mode 	=> 444,
 		owner	=> root,
 		group	=> root,
 	}
 
-        service{ 'php5':
-                  enable  => true,
-                  ensure  => running,
-                  require => Package['php5'],
-        }
-        service{ 'libapache2-mod-php5':
-                  enable  => true,
-                  ensure  => running,
-                  require => Package['libapache2-mod-php5'],
-        }
-        service{ 'php5-mycrypt':
-                  enable  => true,
-                  ensure  => running,
-                  require => Package['php5-mycrypt'],
-        }
 }
